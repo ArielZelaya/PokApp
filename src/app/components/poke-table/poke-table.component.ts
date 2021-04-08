ed0da7c9b82af06b4ff797a4db7acbc42861d177
Ariel Zelaya <cristianarielzelaya@hotmail.com>
2021-04-08 14:29:48 -0700

    Basic-Functions

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -10,7 +10,7 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./poke-table.component.css']
 })
 export class PokeTableComponent implements OnInit {
-  displayedColumns: string[] = ['position','image','name'];
+  displayedColumns: string[] = ['position','image','name','tipo','generacion'];
   data:any[] = [];
   dataSource = new MatTableDataSource<any>(this.data);
   pokemons = [];
@@ -26,13 +26,47 @@ export class PokeTableComponent implements OnInit {
 
   getPokemons(){
     let pokemonData;
-    for(let i = 1; i <= 898; i++){
+   
+
+    /* Para cargar los pokemon itero hasta el numero en la podex deseado  
+    Exa: La tercera generacion llega hasta el numero 386
+    */
+
+    for(let i = 1; i <= 725; i++){
+
+      /* Para obtener la generacion no pude encontrar una forma mediante 
+      la API (de seguro no revise bien) 
+      asi que las defini segun su id en la pokedex */
+      let generation;
+      if(i>=1 && i<=150 ){
+        generation = "primera Generacion";
+      }else if(i>=151 && i<=251){
+        generation = "Segunda Generacion";
+      }else if(i>=252 && i<=386){
+        generation = "Tercera Generacion";
+      }
+      else if(i>=387 && i<=493){
+        generation = "Cuarta Generacion";
+      }
+      else if(i>=494 && i<=649){
+        generation = "Quinta Generacion";
+      }
+      else if(i>=650 && i<=721){
+        generation = "Sexta Generacion";
+      }
+      
       this.pokeService.getPokemons(i).subscribe(
         res =>{
           pokemonData = {
             position: i,
             image: res.sprites.front_default,
-            name: res.name
+            tipo: res.types[0].type.name,
+            name: res.name,
+            
+            
+
+            generacion: generation
+
           }
           this.data.push(pokemonData);
           this.dataSource = new  MatTableDataSource<any>(this.data);
