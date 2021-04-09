4c2a8f4aa367b9be3b4a845d420b4205fe7183fe
Ariel Zelaya <cristianarielzelaya@hotmail.com>
2021-04-09 01:46:15 -0700

    Deploy new Server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// server.js
-const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
